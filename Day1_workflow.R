7f7ce02f7b0568b0ac29ed0ae1d4d930ca5910ca
Benjamin F. Voight <bvoight@upenn.edu>
2021-04-11 18:00:43 -0400

    Update Day1_workflow.R

--- Day1_workflow.R
+++ Day1_workflow.R
@@ -104,8 +104,10 @@ endfix <- ".txt"
 paste(prefix,endfix,sep="")
 myfilename <- paste(prefix,endfix,sep="")
 
-
-
+## another example: see what you have loaded in R. 
+#I include this in all pipelines
+?sessionInfo
+sessionInfo()
 
 ###############################################
 # Challenge Questions for Parts I, II, III, IV 
@@ -146,6 +148,9 @@ myfilename <- paste(prefix,endfix,sep="")
 library(tidyverse)    #load up the tidyverse (for later)
 
 
+
+
+
 ##########################################
 ## VI. Lists, vectors, matrix, data.frame
 #
@@ -157,8 +162,8 @@ v <- c(5,6,7,8)    #vector: a collection of elements of the same type
 v
 
 # you can access contents of this vector with square bracket notation '[]'
-v[1]       # this returns 5, since "5" is values contained in the first element of v
-v[5]       # Whoops! this is out of bounds of the vector
+v[1]       # this returns 5, since "5" is the value contained in the first element of v
+v[5]       # Whoops! this is out of bounds of the vector. "There is no fifth element!"
 length(v)  # helpful to know how many elements are in a list
 
 
@@ -177,7 +182,7 @@ t[1,]      # the first row of the variable t
 t[,1]      # the first column of the variable t, e.g. t$v
 t[c(1,3),] # give me the first and third rows
 
-# You can apply arithmetic to list very easily:
+# You can apply arithmetic to lists very easily:
 v
 v * 2
 v / 4
@@ -187,9 +192,8 @@ v / 4
 #
 # But what you really want to do is determine if entries in a table meet some criteria
 #
-# to do that, let's introduce comparison. There are a number of logical operators
+# to do that, let's introduce comparison. There are a number of logical operators for this.
 #
-# These work if you are a number:
 #
 # ==, !=      "equal to", "not equal to"
 # >, >=       "greater than", "greater than or equal to"
@@ -211,7 +215,7 @@ iris$Sepal.Length > 5.1      # is the element of Sepal.Length greater than 5.1?
 sel <- iris$Sepal.Length == 5.1
 iris[sel,]
 
-iris[iris$Sepal.Length == 5.1,]   #This works too - a little messier, but you avoid creating sel
+iris[iris$Sepal.Length == 5.1,]   #This works too, but you avoid creating sel if you can read this
 
 
 ###############################################
@@ -226,7 +230,7 @@ iris[iris$Sepal.Length == 5.1,]   #This works too - a little messier, but you av
 # 3. Perform a t.test to compare the Sepal Widths of setosa and versicolor. Are these different?
 #
 # Challenge:
-# 4. How many species of virginica have a Petal width greater than 2.0?
+# 4. How many species of virginica have a Petal width greater than 1.5?
 #
 # 5. Create a list of Sepal Length that is standardized for all species (element minus mean divided by standard deviation)
 
@@ -244,7 +248,7 @@ iris[iris$Sepal.Length == 5.1,]   #This works too - a little messier, but you av
 #
 # In RStudio, you can set the 'working directory' easily:
 # 
-# Menu --> Session --> Set Working Directory --> Choose Directory
+# Menu ==> Session ==> Set Working Directory ==> Choose Directory
 #
 # You can see that this actually calls a function:
 #
@@ -252,7 +256,7 @@ iris[iris$Sepal.Length == 5.1,]   #This works too - a little messier, but you av
 # 
 # which takes in the directory you specified as the argument:
 
-setwd("C:/Users/bvoight/Desktop/ITMAT_Workshop")
+# setwd("C:/Users/bvoight/Desktop/ITMAT_Workshop")     #this is specific to my computer, you will have your own for this!
 
 # Now, head to the following link, download the data, and place it into the ITMAT_Workshop
 #
@@ -291,7 +295,7 @@ GTEx_data[1:5,1:5]
 
 
 ##############################################
-# VIII. Crash course in Tidyverse
+# VIII-A. Crash course in Tidyverse
 #
 # Up to this point, we've given you a crash course in "base R"
 #
@@ -306,7 +310,7 @@ library(tidyverse)
 # and data set we wanted
 GTEx_data
 
-# Data comes in different ways, so let's convert it into a "tibble" (tbl, or table)
+# Data comes in different ways, so let's convert it into a "tibble" (tbl, or a tidyverse table)
 # friendly to tidyverse
 tibble(GTEx_data)
 
@@ -322,16 +326,53 @@ GTEx_data_tbl <- tibble(GTEx_data)
 # https://github.com/rstudio/cheatsheets/blob/master/data-transformation.pdf
 # 
 
-mutate()
-select()
-filter()
-summarise()
-arrange()
+# Let's try out a couple of things
+#
+# let's look up my favorite gene - GCKR - and 'filter' it from the Description column
+filter(GTEx_data_tbl, Description == "GCKR")
 
-rename()
+# let's save the output
+expr_gckr <- filter(GTEx_data_tbl, Description == "GCKR")
+
+# that's a lot of tissues. Let's 'select' liver specifically:
+select(expr_gckr, Liver)
+
+# let's get all of the liver data, but sort it
+expr_liver <- select(GTEx_data_tbl, Liver)
+arrange(expr_liver, by=Liver)
+
+# mmm maybe we need to do it in ascerding order
+arrange(expr_liver, by=desc(Liver))
+
+# mmm maybe we need to get the gene names too
+expr_liver <- select(GTEx_data_tbl, Description, Liver)
+arrange(expr_liver, by=desc(Liver))
+
+# Let's change the name of our "description" column to be a bit more specific
+expr_liver <- rename(expr_liver, geneid = Description)
+expr_liver
+
+# let's create a data set that's a little bit bigger
+# and create a column that denotes which genes are higher expressed in liver compared with Pancreas
+expr_liver_panc <- select(GTEx_data_tbl, Description, Liver, Pancreas)
+mutate(expr_liver_panc, liver_gt = Liver > Pancreas)
 
 
+###############################################
+# Break out Questions for Parts VIII-A
+#
+# Using Tidyverse:
+#
+# 1. create a table with the genes in spleen with tpm > 10000. How many genes are there?
+#
+# 2. create a table with median expression of PNPLA3 in: 
+#    Pancreas, Adipose (Subcutaneous), Adipose (visceral), Liver, and Skeletal Muscle
+#    Hint: colnames() can return the names of the columns of a table
 
+
+###############################################
+# VIII-B. Crash course in Tidyverse - 'Ce n'est pas une pipe'
+#
 # Let's start building up our workflows a little bit more.
 # for that, I need to introduce you to another operator:
 #
@@ -341,17 +382,98 @@ rename()
 #
 # This passes the object (noun) on the left side to the function (verb) on the right side
 #
-# x %>% f(y) 		is the same as 			f(x,y)
+# x %>% f(y) 		      is the same as 			f(x,y)
 # y %>% f(x, ., z) 		is the same as 			f(x,y,z)
 #
 # this allows one to "chain" together objects (nouns) to a series of function (verbs)
 # that you want done.
 #
-# e.g., remember the challenge problem 
-# >> 4. How many species of virginica have a Petal width greater than 2.0?
+# e.g., remember the challenge problem above?
+# >> 4. How many species of virginica have a Petal width greater than 1.5?
+# you can do that really easily here:
+iris %>% 
+  filter(Petal.Width > 1.5) %>%
+  filter(Species == "virginica") %>%
+  count()
+
+# it was easy to think about this in multiple steps which you can build up
+# with tidyverse, you can do this step-by-step very easily
+
+# OK, let's reconsider problem #2 that you just solved, above, with tidyverse
+# there you have "two steps" that you want to achieve.
+
+# step one: filter for the gene name
+GTEx_data %>%
+  filter(Description == "PNPLA3")
+
+# step two: select only the columns from this that you want
+GTEx_data %>%
+  filter(Description == "PNPLA3") %>%
+  select(Description, Pancreas, Adipose...Subcutaneous, Adipose...Visceral..Omentum., Liver, Muscle...Skeletal)
+
+# Voila! But let's build it out more.
+# tidy up the column names too while we're at it:
+GTEx_data %>%
+  filter(Description == "PNPLA3") %>%
+  select(Description, Pancreas, Adipose...Subcutaneous, Adipose...Visceral..Omentum., Liver, Muscle...Skeletal) %>%
+  rename(geneid = Description, AdiposeSubCutaneous = Adipose...Subcutaneous, AdiposeVisceral = Adipose...Visceral..Omentum., SkeletalMuscle = Muscle...Skeletal)
+
+# OK but now let's store this output in our variable:
+my_t2d_tbl <- GTEx_data %>%
+              filter(Description == "PNPLA3") %>%
+              select(Description, Pancreas, Adipose...Subcutaneous, Adipose...Visceral..Omentum., Liver, Muscle...Skeletal) %>%
+              rename(geneid = Description, AdiposeSubCutaneous = Adipose...Subcutaneous, AdiposeVisceral = Adipose...Visceral..Omentum., SkeletalMuscle = Muscle...Skeletal)
+
+## What You can see in the above is a couple of things (let me spell it out)
 #
-# it was easy to think about this in multiple steps, but you can build that up
-# with tidyverse very easily
+# 1. we built a series of steps, one a time, looking at the output each time
+#    to make sure that we had what we thought we had.
+#    "Code a little, debug a lot"
+#    "Trust, but verify" --Suzanne Massie 
+#
+# 2. The code here is 'clean' and is /readable/, once you get used to reading it
+#    "objects" ==> Nouns, "functions" ==> verbs
+#    "take this thing, do this to it. On the output, do this to it. And then this." etc.
+#
+# 3. There's strictly speaking no limit on the steps you could take here.
+#    It can be handy to add comments to various lines so that a user knows what you were
+#    intending.
+
+# One final thing. Really you didn't want a single gene, you had a list of genes you were interested in:
+t2d_genelist <- c("TCF7L2", "MC4R", "SLC30A8", "FTO", "PNPLA3")
+
+# let's get 'em all then using the %in% operator
+# OK but now let's store this output in our variable:
+my_t2d_tbl <- GTEx_data %>%
+  filter(Description %in% t2d_genelist) %>%
+  select(Description, Pancreas, Adipose...Subcutaneous, Adipose...Visceral..Omentum., Liver, Muscle...Skeletal) %>%
+  rename(geneid = Description, AdiposeSubCutaneous = Adipose...Subcutaneous, AdiposeVisceral = Adipose...Visceral..Omentum., SkeletalMuscle = Muscle...Skeletal)
+my_t2d_tbl
+
+# Magnifique!!
+
+#########################################
+## Break out Questions for Parts VIII-B
+#
+# 1. Above, you can see that MC4R is not well express in any of these tissues.
+#    Identify the tissue with the highest expression of MC4R, and add that tissue to your table!
+
+
+##########################
+## END OF WORKSHOP DAY 1##
+##########################
+
+
+
+##############################################
+###### SPOILER ALERT -- ANSWERS BELOW ########
+##############################################
+
+
+
+
+
+
 
 
 
@@ -396,9 +518,9 @@ veronly <- iris[iris$Species == "versicolor",]
 t.test(sonly$Sepal.Width,veronly$Sepal.Width)
 
 # Challenge:
-# 4. How many species of virginica have a Petal width greater than 2.0?
+# 4. How many species of virginica have a Petal width greater than 1.5?
 vironly <- iris[iris$Species == "virginica",]
-vironly <- vironly[vironly$Petal.Width > 2,]
+vironly <- vironly[vironly$Petal.Width > 1.5,]
 length(vironly[,1])
 
 # 5. Create a list of Sepal Length that is standardized for all species (element minus mean divided by standard deviation)
@@ -410,7 +532,7 @@ sd(std_sl)
 
 
 #######################
-# Break-out Questions for VII
+# Answers to Break-out Questions for VII
 #
 # 1a. Download the following and place into your directory
 #  
@@ -424,4 +546,35 @@ read.table(file="genes.table",sep=" ",header=T)
 # 2. In GTEx data, which genes have greater tpm than 10000 in Liver? Anything you notice in common?
 GTEx_data[GTEx_data$Liver > 10000,]
 # Yep, MT suggest mitochondrial data. That said, HP and ALB are good positive controls too.
+
+
+###############################################
+# Answers to Break out Questions for Parts VIII-A
+#
+# Using Tidyverse:
+#
+# 1. create a table with the genes in spleen with tpm > 10000. How many genes are there?
+# 12 genes
+top_expr_spleen <- filter(GTEx_data_tbl, Spleen > 10000)
+
+# 2. create a table with median expression of PNPLA3 in: 
+#    Pancreas, Adipose (Subcutaneous), Adipose (visceral), Liver, and Skeletal Muscle
+#    Hint: colnames() can return the names of the columns of a table
+my_t2d_tbl <- filter(GTEx_data_tbl, Description == "PNPLA3")
+my_t2d_tbl <- select(my_t2d_tbl, Description, Pancreas, Adipose...Subcutaneous, Adipose...Visceral..Omentum., Liver, Muscle...Skeletal)
+
+#########################################
+## Break out Questions for Parts VIII-B
+#
+# 1. Above, you can see that MC4R is not well express in any of these tissues.
+#    Identify the tissue with the highest expression of MC4R, and add that tissue to your table!
+mc4r_expr <- GTEx_data %>% filter(Description == "MC4R")
+max(mc4r_expr[,3:56])                      ## When you look, it's "Brain...Hypothalamus" that has it
+t(mc4r_expr) == max(mc4r_expr[,3:56])      ## this makes it a little easier to see
+
+my_t2d_tbl <- GTEx_data %>%
+  filter(Description %in% t2d_genelist) %>%
+  select(Description, Pancreas, Adipose...Subcutaneous, Adipose...Visceral..Omentum., Liver, Muscle...Skeletal, Brain...Hypothalamus) %>%
+  rename(geneid = Description, AdiposeSubCutaneous = Adipose...Subcutaneous, AdiposeVisceral = Adipose...Visceral..Omentum., SkeletalMuscle = Muscle...Skeletal, BrainHypoT=Brain...Hypothalamus)
+my_t2d_tbl
 
