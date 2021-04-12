60fe61a72e6f6cffb1cbb472c7745150756b7283
Benjamin F. Voight <bvoight@upenn.edu>
2021-04-11 22:53:30 -0400

    Update Day2_workflow.R

--- Day2_workflow.R
+++ Day2_workflow.R
@@ -33,6 +33,41 @@
 # github desktop on your machine:
 # https://desktop.github.com/
 #
+####
+#
+
+## First, let's do a little bit of setup.
+#
+# let's set our working directory to our workspace (I had mine on my desktop)
+# setwd("C:/Users/bvoight/Desktop/ITMAT_Workshop")
+
+# let's also load a library that we'll need: tidyverse
+library(tidyverse)
 
 ############################
-## I. 
+## I. Your first repository
+#
+# I personally think the *easiest* way to create a new repository
+# is to do it on your account on github
+#
+# then, clone that onto your machine and add things accordingly
+#
+# https://github.com/ -> Log in -> click on '+' icon in upper left 
+# -> new repository 
+#
+# Name it what you'd like
+#
+# Give it a description
+#
+# Let's make this one 'private' for now (you can change this)
+#
+# Click the boxes "Add a README.md"
+#
+# "Choose a License" -> MIT License
+
+# Great! You've made your first repo. Now, let's place this onto your
+# computer.
+#
+# Open 
+
+
